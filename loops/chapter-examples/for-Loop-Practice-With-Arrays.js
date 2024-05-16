9f2678519ecd64144a98f7b3d27373eb48627c23
jyothibhavani <jyothibhavani.illa@slu.edu>
2024-05-16 17:07:43 -0500

    loops chapter-examples

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,10 @@
 // create an array variable containing the names
 
+let nameArray = ["Jyothi", "Bhavani", "Tarun", "Venkat"];
+
 // write a for loop that prints each name on a different line
+
+for(let i =0;i<nameArray.length;i++)
+    {
+        console.log(nameArray[i]);
+    }
