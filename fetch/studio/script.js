15fea349ae6f16197c8ff9abb538fcfab00e81df
jyothibhavani <jyothibhavani.illa@slu.edu>
2024-08-15 20:05:33 -0500

    Fetch studio

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,26 @@
 //TODO: Add Your Code Below
+window.addEventListener("load", function(){
+fetch("https://handlers.education.launchcode.org/static/astronauts.json")
+.then(function(response){
+response.json().then(function(json){
+const container = document.getElementById("container");
+json.sort((a,b) => b.hoursInSpace - a.hoursInSpace);
+container.innerHTML = `<h3>Astronauts count: ${json.length}</h3>`
+for(let i =0 ;i <json.length; i++)
+{
+    container.innerHTML += `<div class="astronaut">
+    <div class="bio">
+        <h3>${json[i].firstName} ${json[i].lastName}</h3>
+        <ul>
+        <li>Hours in space: ${json[i].hoursInSpace}</li>
+        <li class = "${json[i].active ? 'status': ''}">Active: ${json[i].active}</li>
+        <li>Skills: ${json[i].skills}</li>
+        </ul>
+    </div>
+    <img class="avatar" src=${json[i].picture}>
+</div>`;
+}
+
+});
+});
+});
