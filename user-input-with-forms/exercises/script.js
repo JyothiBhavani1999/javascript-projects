8e0c8482028cac61c5c655a31d2b1a490ce8f4ac
jyothibhavani <jyothibhavani.illa@slu.edu>
2024-08-21 03:16:26 -0500

    forms exercise

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,18 @@
 //Code Your Solution Below
+window.addEventListener("load", ()=> {
+let form = document.querySelector("form");
+form.addEventListener("submit", function(event){
+let testName = document.querySelector("input[name=testName]");
+let testDate = document.querySelector("input[name=testDate]");
+let rocketType = document.querySelector("input[name=rocketType]");
+let boosterCount = document.querySelector("input[name=boosterCount]");
+let windRating = document.querySelector("input[name=windRating]");
+let productionServers = document.querySelector("input[name=productionServers]");
+
+if(testName.value === '' || testDate.value === '' || rocketType.value === '' || boosterCount.value === '' || windRating.value === ''|| productionServers.value === '' )
+{
+    alert('All fields are required!');
+    event.preventDefault();
+}
+});
+});
